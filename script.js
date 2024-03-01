d38ce16c8b786cd1de1880bbfe875f5b74799205
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-03-01 19:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function insert_Row() {
     //Write your code here
   
-  
+  const table=document.getElementById('sampleTable');
+	var newRow = table.insertRow(0);
+
+	const tr=document.createElement('tr');
+	const td1=document.createElement('td');
+	const td2=document.createElement('td');
+	td1.innerText='New Cell1';
+	td2.innerText='New Cell2';
+
+	newRow.append(td1);
+	newRow.append(td2);
+	//table.(tr);
+	
+	
 }
